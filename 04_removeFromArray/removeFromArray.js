49f1244c82741997143976b86034c14c9bc4f514
decio88 <decio88@users.noreply.github.com>
2023-01-14 19:24:26 +0100

    solution to exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, x, y, z, a) {
+  for (let i = 0; i < array.length; ) {
+    let element = array[i];
+    if (element === x || element === y || element === z || element === a) {
+      array.splice(array.indexOf(element), 1);
+    } else {
+      i++;
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
