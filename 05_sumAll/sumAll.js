93bc67c91b426b30bde4b77cebfdaa975a3892e6
decio88 <decio88@users.noreply.github.com>
2023-01-14 19:43:47 +0100

    solution to exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+  if (x < 0 || y < 0 || typeof x != "number" || typeof y != "number") {
+    return "ERROR";
+  }
+  let result = Math.min(x, y);
+  for (let i = Math.min(x, y); i <= Math.max(x, y); i++) {
+    result += i;
+  }
+  return result - 1;
 };
 
 // Do not edit below this line
