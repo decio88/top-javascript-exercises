519232b1e143f2f71668f1fb00a002d8f94c79f9
decio88 <decio88@users.noreply.github.com>
2023-01-18 18:15:48 +0100

    Solution to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+// const palindromes = (sentence) =>
+//   sentence
+//     .toLowerCase()
+//     .replace(/[^a-zA-Z0-9 ]/g, "")
+//     .split("")
+//     .reverse()
+//     .join("") === sentence;
 
+const palindromes = function (sentence) {
+  let clean = sentence.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  return clean.split("").reverse().join("") === clean;
 };
 
 // Do not edit below this line
