000c1c4750a435f985e55abcedc5672de50e96f2
decio88 <decio88@users.noreply.github.com>
2023-01-18 18:55:25 +0100

    Resolved fibonacci sequence ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (x) {
+  x = +x;
+  const fibo = [1];
+  if (x <= 0) {
+    return "OOPS";
+  }
 
+  for (let i = 0; i <= x; i++) {
+    let num;
+    if (i === 0) {
+      num = 1;
+    } else {
+      num = fibo[i] + fibo[i - 1];
+    }
+
+    fibo.push(num);
+  }
+
+  return fibo[x - 1];
 };
 
 // Do not edit below this line
